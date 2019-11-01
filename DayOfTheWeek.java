2a10fd6f35d635a58170d99f707f706b95e57a05
maschaalbu <56225985+maschaalbu@users.noreply.github.com>
2019-11-01 17:22:37 +0100

    Create DayOfTheWeek.java

---
+++ DayOfTheWeek.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DayOfTheWeek {
+
+	public static void main(String[] args) {
+		
+		String[] week = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+		
+		System.out.println("Please enter the number from 1 to 7: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		
+		scanner.close();
+		
+		for(int i = 0; i < week.length; i++) {
+			
+			if(number == i+1) {
+				
+				System.out.print(week[i]);
+			}
+			
+		}
+		
+	}
+	
+}
