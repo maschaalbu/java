3984612b941734b28b0b681513042754a825de38
maschaalbu <56225985+maschaalbu@users.noreply.github.com>
2019-11-24 15:03:16 +0100

    add getter and setter for calories

--- Fruit.java
+++ Fruit.java
@@ -4,6 +4,14 @@ public class Fruit {
 	
 	private double calories;
 	
+	public double getCalories() {
+		return calories;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+	
 	public void makeJuice() {
 		
 		System.out.println("juice is made");
